6c37be0706353ab8b17149709d868cf6259a9369
Logan Howlett <logan@weaponxi.com>
2013-03-01 19:12:17 -0500

    added line break eliminator

--- exec_all.cpp
+++ exec_all.cpp
@@ -176,11 +176,39 @@ Handle<Value> executeArray(const Arguments& args) {
 
       while(fgets(buff, sizeof(buff), in)!=NULL){
         std::cout << buff;
+
+        //clear output
         cmd_output.str(std::string());
+        // stream buffer to output
         cmd_output << buff;
+        std::string str;
+        str = cmd_output.str();
+
+        // //test
+        // //last line
+        // std::cout << "\nlast line: \n'"
+        // << str[str.length()]
+        // << "'";
+        
+        // std::cout << "\nlast line -1: \n'"
+        // << str[str.length() -1 ]
+        // << "'";
+
+        // std::cout << "\nlast line -2: \n'"
+        // << str[str.length() -2 ]
+        // << "'";
+
+
+
+        if (str[str.length()-1] == '\n')
+        {
+          str.erase(str.length()-1);
+        }
         //testing
-        std::cout << CYAN << "\n buffered output \n"<< cmd_output.str()
-      << "\n length: " << cmd_output.str().length() << "\n" << RESET;
+        std::cout 
+          << CYAN << "\n buffered output \n'"<< str
+          << "'\n length: " << cmd_output.str().length()
+          << "/" << str.length() << "\n" << RESET;
       }
       // std::cout << "\n buffered output \n"<< cmd_output.str()
       // << "\n length: " << cmd_output.str().length();
